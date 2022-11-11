51ff0beb301ef3cc058eaa5baf35f8561d657cb5
dav100ide <oreglio.dev@gmail.com>
2022-11-11 13:41:41 +0100

    fine consegna esercizio

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ createApp({
    data() {
       return {
          message: 'questo è il mio primo messaggio di vue',
+         imgSrc: 'https://picsum.photos/600/300',
       };
    },
 }).mount('#app');
